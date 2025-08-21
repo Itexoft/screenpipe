88b5f513c3faaf2e788755baf89bd93b55280272
Denis Kudelin <15978569+denis-kudelin@users.noreply.github.com>
2025-08-21 03:11:36 +0300

    Use VC++ redistributable installer

--- screenpipe-app-tauri/src-tauri/build.rs
+++ screenpipe-app-tauri/src-tauri/build.rs
@@ -2,9 +2,9 @@ fn main() {
     tauri_build::build();
     #[cfg(target_os = "windows")]
     {
-        let has = glob::glob("vcredist/*.dll").unwrap().next().is_some();
+        let has = glob::glob("vcredist/vc_redist.x64.exe").unwrap().next().is_some();
         if !has {
-            panic!("vcredist dlls not found");
+            panic!("vcredist not found");
         }
     }
 }
